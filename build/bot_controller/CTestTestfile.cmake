cae9f64c94634d42a76dc70ce661262dc410da50
sarinann <sarin.ann10@gmail.com>
2022-12-15 16:18:01 -0500

    odom

--- build/bot_controller/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ab/ros2_ws/src/809y_project/final/bot_controller
-# Build directory: /home/ab/ros2_ws/src/809y_project/build/bot_controller
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
