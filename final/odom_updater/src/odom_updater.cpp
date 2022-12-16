547dbbd9e0a36e4cf50962052e275143169e03b3
sarinann <sarin.ann10@gmail.com>
2022-12-16 11:09:50 -0500

    error

--- final/odom_updater/src/odom_updater.cpp
+++ final/odom_updater/src/odom_updater.cpp
@@ -55,10 +55,10 @@
 
 void odom_updater::timer_callback(const std::shared_ptr<nav_msgs::msg::Odometry> msg)
 {
-    broadcast_odom(auto const std::shared_ptr<nav_msgs::msg::Odometry> msg);
+    broadcast_odom(auto std::shared_ptr<nav_msgs::msg::Odometry> msg);
 }
 
-void odom_updater::broadcast_odom(const std::shared_ptr<nav_msgs::msg::Odometry> msg)
+void odom_updater::broadcast_odom(suto std::shared_ptr<nav_msgs::msg::Odometry> msg)
 {
     geometry_msgs::msg::TransformStamped t;
 
