b7fb38a56fd57161710031d3eff3c5d4c7f19745
sarinann <sarin.ann10@gmail.com>
2022-12-16 14:02:17 -0500

    only commit

--- final/target_reacher/src/target_reacher.cpp
+++ final/target_reacher/src/target_reacher.cpp
@@ -1,4 +1,102 @@
 #include <rclcpp/rclcpp.hpp>
-#include "target_reacher/target_reacher.h"
+#include "target_reacher.h"
+
+void TargetReacher::final_destination(int goal)
+{
+        geometry_msgs::msg::TransformStamped g;
+
+        g.header.stamp = this->get_clock()->now();
+        g.header.frame_id = fd_frame_id;
+        g.child_frame_id = "final_destination";
+
+        if (goal==0)
+        {
+            g.transform.translation.x = aruco_0_x;
+            g.transform.translation.y = aruco_0_y;
+            g_rcutils_logging_initialized.transform.translation.z = 0.0;
+        }else if (goal==1)
+        {
+            g.transform.translation.x = fd_aruco_1_x;
+            g.transform.translation.y = fd_aruco_1_y;
+            g.transform.translation.z = 0.0;
+        }else if (goal==2)
+        {
+            g.transform.translation.x = fd_aruco_2_x;
+            g.transform.translation.y = fd_aruco_2_y;
+            g.transform.translation.z = 0.0;
+        }else if (goal==3)
+        {
+            g.transform.translation.x = fd_aruco_3_x;
+            g.transform.translation.y = fd_aruco_3_y;
+            g.transform.translation.z = 0.0;
+        } 
+
+        g.transform.rotation.x = 0.0;
+        g.transform.rotation.y = 0.0;
+        g.transform.rotation.z = 0.0;
+        g.transform.rotation.w = 1.0;
+
+        broadcaster->sendTransform(g);
+
+        i=true;
+}
+
+void TargetReacher::cb1(const std::shared_ptr<ros2_aruco_interfaces::msg::ArucoMarkers> aruco)
+{
+    auto x = aruco->marker_ids;
+    if (x.at(0)==0){
+        auto goal=0;
+        final_destination(goal);
+    }else if (x.at(0)==1){
+        auto goal=1;
+        final_destination(goal);
+    }else if (x.at(0)==2){
+        auto goal=2;
+        final_destination(goal);
+    }else if (x.at(0)==3){
+        auto goal=3;
+        final_destination(goal);
+    }
+}
+
+
+void TargetReacher::timer_callback(const std::shared_ptr<std_msgs::msg::Bool> msg)
+{
+    if (msg->data)
+    {
+        geometry_msgs::msg::Twist vel;
+        vel.linear.x = 0;
+        vel.angular.z = 0.2;
+        m_publisher_cmd_vel->publish(vel);
+
+        ros2_aruco_interfaces::msg::ArucoMarkers aruco;
+
+    }
+}
+
+void TargetReacher::cb2()
+{
+    if (i==true)
+    {
+        geometry_msgs::msg::TransformStamped t;
+        // Look up for the transformation between "robot1/odom" and "final_destination" frames
+        try
+        {
+            t = m_tf_buffer->lookupTransform("robot1/odom", "final_destination", tf2::TimePointZero);
+        }
+        catch (const tf2::TransformException &ex)
+        {
+            RCLCPP_INFO(
+                this->get_logger(), "Could not transform %s to %s: %s",
+                "robot1/odom", "final_destination", ex.what());
+            return;
+        }
+
+        RCLCPP_INFO(
+            this->get_logger(), "Moving to goal [%f, %f]", t.transform.translation.x, t.transform.translation.y);
+
+        m_bot_controller->set_goal(t.transform.translation.x, t.transform.translation.y);
+    }
+}
 
 
