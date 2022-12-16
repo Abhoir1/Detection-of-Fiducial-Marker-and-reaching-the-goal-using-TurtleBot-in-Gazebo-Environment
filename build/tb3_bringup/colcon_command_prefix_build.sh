c74c6d0c8d46bf9ba436c566d7d8a66fde448d60
Abhoir1 <aditibhoir7276@gmail.com>
2022-12-16 14:01:35 -0500

    robot moves

---
+++ build/tb3_bringup/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/ab/ros2_ws/src/809y_project/install/tb3_gazebo/share/tb3_gazebo/package.sh"
---
+++ build/tb3_gazebo_spawner/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/ab/ros2_ws/src/809y_project/install/tb3_gazebo_spawner'
