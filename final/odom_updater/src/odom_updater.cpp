3136cb0bb8e3f278a1cf8c79bf3da9efb43d1114
Abhoir1 <aditibhoir7276@gmail.com>
2022-12-15 19:04:10 -0500

    added temp updater

--- final/odom_updater/src/odom_updater.cpp
+++ final/odom_updater/src/odom_updater.cpp
@@ -1,24 +1,91 @@
-#pragma once
+// #pragma once
 
-#include <rclcpp/rclcpp.hpp>
-#include <geometry_msgs/msg/twist.hpp>
+// #include <string>
+// #include <rclcpp/rclcpp.hpp>
+// #include <geometry_msgs/msg/twist.hpp>
+// #include <string>
+// #include "bot_controller/bot_controller.h"
+// #include "ros2_aruco_interfaces/msg/aruco_markers.hpp"
+// #include "tf2_ros/static_transform_broadcaster.h"
+
+// // timer
+// class OdomUpdater : public rclcpp::Node
+// {
+// public:
+//     FramePublisher()
+//         : Node("odom_updater")
+//     // OdomUpdater(std::shared_ptr<BotController> const &bot_controller) : Node("target_reacher")
+//     {
+//         tf_broadcaster = std :: make_unique<tf2_ros::StaticTransformBroadcaster>(this);
+//         m_timer = this->create_wall_timer(std::chrono::milliseconds((int)(1000.0 / 10)),
+//                                           std::bind(&FramePublisher::timer_callback, this));
+
+//     }
+// private:
+//     void timer_callback()
+
+//     {
+
+//         geometry_msgs::msg::TransformStamped t;
+        
+//         t.header.stamp = this->get_clock()->now();
+//         t.header.frame_id = odom;
+//         t.child_frame_id = "/robot1/base_footprint";
+
+//         m_tf_broadcaster ->sendTransform();
+
+//     }
+//     std::shared_ptr<tf2_ros::StaticTransformBroadcaster> m_tf_broadcaster;
+//     rclcpp::TimerBase::SharedPtr m_timer;
+// };
+
+// int main(int argc, chae *argv[])
+// {
+//     rclcpp::init(argc, argv);
+//     rclcpp::spin(std::make_shared<FramePublisher>());
+//     rclcpp::shutdown();
+//     return 0;
+// }
+
 #include <string>
-#include "bot_controller/bot_controller.h"
-#include "ros2_aruco_interfaces/msg/aruco_markers.hpp"
-#include "tf2_ros/static_transform_broadcaster.h"
+#include "odom_updater.h"
+#include "tf2/LinearMath/Quaternion.h"
+#include "geometry_msgs/msg/pose.hpp"
+#include "rclcpp/rclcpp.hpp"
 
-// timer
-class OdomUpdater : public rclcpp::Node
+void OdomUpdater::timer_callback()
 {
-public:
-    OdomUpdater(std::shared_ptr<BotController> const &bot_controller) : Node("target_reacher")
-    {
+    broadcast_odom();
+}
 
-        m_bot_controller = bot_controller;
+void OdomUpdater::broadcast_odom()
+{
+    geometry_msgs::msg::TransformStamped t;
 
-    }
+    std::string odom = "/robot1/odom";
+    std::string footprint = "/robot1/base_footprint";
 
-private:
-    // attributes
-    std::shared_ptr<BotController> m_bot_controller;
-};
+
+    /*******************************************
+     * static broadcaster: "world" -> "/robot1/odom"
+     *******************************************/
+    t.header.stamp = this->get_clock()->now();
+    t.header.frame_id = odom;
+    t.child_frame_id = footprint;
+
+    t.transform.translation.x = 0;
+    t.transform.translation.y = 0;
+    t.transform.translation.z = 0;
+
+    tf2::Quaternion q;
+    q.setRPY(0, 0, 0);
+    t.transform.rotation.x = q.x();
+    t.transform.rotation.y = q.y();
+    t.transform.rotation.z = q.z();
+    t.transform.rotation.w = q.w();
+
+    // Send the transformation
+    m_tf_broadcaster->sendTransform(t);
+
+}
+
