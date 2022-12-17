bd8632468b20291dc1c1474658a633ddc5d75603
Abhoir1 <aditibhoir7276@gmail.com>
2022-12-16 23:00:50 -0500

    hh

---
+++ build/bot_controller/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ab/final_ws/src/enpm809y_FinalFall2022/final/bot_controller
+# Build directory: /home/ab/final_ws/build/bot_controller
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/ros2_aruco/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/ab/final_ws/install/ros2_aruco'
